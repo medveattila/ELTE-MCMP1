da06428b43ad98cb58be976a2b2719999416eef5
Attila Medveczky <medve413@gmail.com>
2021-03-10 18:51:37 +0100

    add failsafe for the f'(x) = 0 case

--- HF1/main_hf1.cpp
+++ HF1/main_hf1.cpp
@@ -4,10 +4,26 @@
 template <typename F1, typename F2, typename T>
 T Newton(F1 fun, F2 dif, T x0) {
     
+    double eps = 0.0000001;
+    
+    if (dif(x0) < eps) {
+        
+        std::cout << "Error: Derivate of the function is 0.\n";
+        return 0;
+        
+    }
+    
     double x1 = x0 - (fun(x0)/dif(x0));
     
-    while (fabs(x1-x0) > 0.0000001) {
+    while (fabs(x1-x0) > eps) {
 
+        if (dif(x1) < eps) {
+            
+            std::cout << "Error: Derivate of the function is 0.\n";
+            return 0;
+            
+        }
+        
         x0 = x1;
         
         x1 = x0 - (fun(x0)/dif(x0));
